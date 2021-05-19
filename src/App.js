bf12c36ea377c1462f25cc61bbf5014a31013c83
Aaquib <mdaaquibfaiyaz2015@gmail.com>
2021-05-19 22:50:17 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StateWise from './components/StateWise';
+
+function App() {
+  return (
+    <div className="App">
+      <StateWise />
+    </div>
+  );
+}
+
+export default App;
